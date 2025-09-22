952a5a6dc4cf1b7bdab1d6e4b64bd4a442a44586
Jessie Punayan <jccirs09@gmail.com>
2025-09-21 18:45:16 -0700

    InitialCreateMigration

--- Migrations/20250922014426_InitialCreate.Designer.cs
+++ Migrations/20250922014426_InitialCreate.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CMetalsWS.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20250922013642_AddShiftsAndMachineAssignments")]
-    partial class AddShiftsAndMachineAssignments
+    [Migration("20250922014426_InitialCreate")]
+    partial class InitialCreate
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
